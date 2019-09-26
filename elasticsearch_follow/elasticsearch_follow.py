623dfdd5ea643643325c2f23e49c69768e8f74e5
Marc Schiereck <mdreem@fastmail.fm>
2019-09-26 22:38:15 +0200

    renaming

--- elasticsearch_follow/elasticsearch_follow.py
+++ elasticsearch_follow/elasticsearch_follow.py
@@ -4,12 +4,12 @@ from functools import total_ordering
 
 @total_ordering
 class Entry:
-    def __init__(self, timestamp, id):
+    def __init__(self, timestamp, entry_id):
         self.timestamp = timestamp
-        self.id = id
+        self.entry_id = entry_id
 
     def __eq__(self, other):
-        return (self.timestamp, self.id) == (other.timestamp, other.id)
+        return (self.timestamp, self.entry_id) == (other.timestamp, other.entry_id)
 
     def __lt__(self, other):
         return self.timestamp < other.timestamp
@@ -39,12 +39,12 @@ class ElasticsearchFollow:
                              scroll='2m',
                              doc_type='doc',
                              body=query_since)
-        sid = res['_scroll_id']
+        scroll_id = res['_scroll_id']
         hits = res['hits']['hits']
 
         while res['hits']['hits']:
-            res = self.es.scroll(scroll_id=sid, scroll='2m')
-            sid = res['_scroll_id']
+            res = self.es.scroll(scroll_id=scroll_id, scroll='2m')
+            scroll_id = res['_scroll_id']
             current_hits = res['hits']['hits']
             if current_hits:
                 hits = hits + current_hits
@@ -56,12 +56,12 @@ class ElasticsearchFollow:
         lines = []
 
         for entry in entries:
-            id = entry['_id']
-            if id not in self.added_entries:
+            entry_id = entry['_id']
+            if entry_id not in self.added_entries:
                 new_line = entry['_source']
                 lines.append(new_line)
-                self.entries_by_timestamp.add(Entry(id, new_line[self.timestamp_field]))
+                self.entries_by_timestamp.add(Entry(entry_id, new_line[self.timestamp_field]))
 
-                self.added_entries.add(id)
+                self.added_entries.add(entry_id)
 
         return lines
