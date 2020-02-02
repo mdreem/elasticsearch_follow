9f52a0450fb40a78d57564affc2e200c06a9d6e6
Marc Schiereck <mdreem@fastmail.fm>
2020-02-02 22:20:50 +0100

    raise version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="elasticsearch_follow",
-    version="0.1.0",
+    version="0.1.1",
     author="Marc Schiereck",
     author_email="mdreem@fastmail.fm",
     description="An Elasticsearch tail",
