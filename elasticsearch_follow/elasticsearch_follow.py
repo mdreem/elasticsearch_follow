152f42f83a4d5bafd31e883df8013af5a4b176aa
Marc Schiereck <mdreem@fastmail.fm>
2020-02-03 21:40:08 +0100

    add docstrings

--- elasticsearch_follow/elasticsearch_follow.py
+++ elasticsearch_follow/elasticsearch_follow.py
@@ -21,6 +21,12 @@ class Entry:
 
 class ElasticsearchFollow:
     def __init__(self, elasticsearch, timestamp_field='@timestamp', query_string=None):
+        """
+        :param elasticsearch: elasticsearch instance from the ``elasticsearch``-library.
+        :param timestamp_field: Denotes which field in the elasticsearch-index is used
+            as the timestamp.
+        :param query_string: The query used to fetch data from elasticsearch.
+        """
         self.es = elasticsearch
         self.timestamp_field = timestamp_field
 
@@ -42,6 +48,14 @@ class ElasticsearchFollow:
             })
 
     def get_entries_since(self, index, timestamp):
+        """
+        Yield all entries since ``timestamp`` until now from ``index``.
+
+        :param index: Elasticsearch index from which new entries should
+            be retrieved. Wildcards are supported.
+        :param timestamp: Timestamp from whoch to start fetching lines.
+        :return: Yields entries until no entries are left.
+        """
         query_since = deepcopy(self.base_query)
         query_since['query']['bool']['must'].append({
             'range': {
@@ -70,6 +84,15 @@ class ElasticsearchFollow:
         self.es.clear_scroll(scroll_id=scroll_id)
 
     def get_new_lines(self, index, timestamp):
+        """
+        Retrieves new lines starting with timestamp until now.
+        Only yield entries which have not yet been fetched from elasticsearch.
+
+        :param index: Elasticsearch index from which new entries should
+            be retrieved. Wildcards are supported.
+        :param timestamp: Timestamp from whoch to start fetching lines.
+        :return: Yields the new lines until the list is empty.
+        """
         entries = self.get_entries_since(index, timestamp)
 
         for entry in entries:
@@ -85,6 +108,10 @@ class ElasticsearchFollow:
                 yield new_line
 
     def prune_before(self, timestamp):
+        """
+        Removes All entries before  ``timestamp`` from the internal buffer.
+        :param timestamp: All entries in the internal before this timestamp will be removed.
+        """
         while len(self.entries_by_timestamp) > 0:
             oldest_entry = heapq.heappop(self.entries_by_timestamp)
             if oldest_entry.timestamp <= timestamp:
