aaf1d8c44356ec8ddc2bd00407b5d6008c0193c7
Marc Schiereck <mdreem@fastmail.fm>
2020-03-02 20:48:35 +0100

    unittest for formatting processor

---
+++ tests/test_formatting_processor.py
@@ -0,0 +1,32 @@
+import unittest
+from unittest.mock import Mock
+
+import elasticsearch_follow
+
+
+class TestDefaultProcessor(unittest.TestCase):
+    def test_formatting_processor_formats(self):
+        processor = elasticsearch_follow.FormattingProcessor(format_string='{@timestamp} {msg}')
+        es_follow = Mock()
+        es_follow.get_new_lines.return_value = [{
+            'msg': 'line1',
+            '@timestamp': '2019-01-01T10:01:00'
+        }]
+        follower = elasticsearch_follow.Follower(es_follow, 'some_index', 120, processor)
+
+        generator = follower.generator()
+
+        self.assertEqual('2019-01-01T10:01:00 line1', next(generator))
+
+    def test_formatting_processor_skips_missing_fields(self):
+        processor = elasticsearch_follow.FormattingProcessor(format_string='->{@timestamp} {unknown}<-')
+        es_follow = Mock()
+        es_follow.get_new_lines.return_value = [{
+            'msg': 'line1',
+            '@timestamp': '2019-01-01T10:01:00'
+        }]
+        follower = elasticsearch_follow.Follower(es_follow, 'some_index', 120, processor)
+
+        generator = follower.generator()
+
+        self.assertEqual('->2019-01-01T10:01:00 <-', next(generator))
